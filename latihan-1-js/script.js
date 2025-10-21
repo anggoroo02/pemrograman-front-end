5990cd76547bf53dbc650c01fcc6d7d23ef7c41a
Anggoro Jati 2 <143049606+anggoroo02@users.noreply.github.com>
2025-10-21 09:38:21 +0700

    latihan 1 js
    buat tampilan form registrasi terdiri dari - nama depan - nama belakang - button ketika button di klik, munculkan log nama lengkap: [nama depan] [nama belakang]

---
+++ latihan-1-js/script.js
@@ -0,0 +1,5 @@
+function showFullName() {
+      const firstName = document.getElementById("firstName").value;
+      const lastName = document.getElementById("lastName").value;
+      console.log(`nama lengkap: ${firstName} ${lastName}`);
+    }
