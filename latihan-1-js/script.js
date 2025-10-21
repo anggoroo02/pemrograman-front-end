5bf434f3391634a9a092d901acbccf9764b5b3d8
Anggoro Jati 2 <143049606+anggoroo02@users.noreply.github.com>
2025-10-21 09:47:11 +0700

    Update script.js

--- latihan-1-js/script.js
+++ latihan-1-js/script.js
@@ -1,5 +1,5 @@
 function showFullName() {
-      const firstName = document.getElementById("firstName").value;
-      const lastName = document.getElementById("lastName").value;
+      const firstName = document.getElementById("Anggoro Jati").value;
+      const lastName = document.getElementById("Pratikto").value;
       console.log(`nama lengkap: ${firstName} ${lastName}`);
     }
